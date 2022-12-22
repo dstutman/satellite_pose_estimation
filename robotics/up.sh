81967d6ccfce1addc797bc090dcd3da63bf31af2
Daniel Avishai Stutman <git@stutman.tech>
2022-12-22 16:27:42 +0100

    Add shebang

--- robotics/up.sh
+++ robotics/up.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Starts dependency service ROS containers
 podman run --net=host --env ROS_HOSTNAME=fedora.local --env ROS_PORT=11311 -d ros:noetic-ros-core roscore
 podman run --net=host --env ROS_MASTER_URI="https://fedora.local:11311" --privileged -d localhost/daheng_camera:latest
